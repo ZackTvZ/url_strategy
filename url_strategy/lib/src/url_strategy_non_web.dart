92570134116ea62d4625b1188528391d13398bc7
Zack <zacktvz@hotmail.my>
2023-06-19 16:26:26 +0800

    update

--- url_strategy/lib/src/url_strategy_non_web.dart
+++ url_strategy/lib/src/url_strategy_non_web.dart
@@ -21,3 +21,15 @@ void setPathUrlStrategy() {
 void setHashUrlStrategy() {
   // Noop.
 }
+
+/// Sets the URL strategy of your web app to using a leading has (`#`) instead
+/// of paths.
+///
+/// You can safely call this on all platforms, i.e. also when running on mobile
+/// or desktop. In that case, it will simply be a noop.
+///
+/// See also:
+///  * [setPathUrlStrategy], which will use a path URL strategy instead.
+void setNoPathUrlStrategy() {
+  // Noop.
+}
--- url_strategy/lib/src/url_strategy_web.dart
+++ url_strategy/lib/src/url_strategy_web.dart
@@ -23,3 +23,29 @@ void setPathUrlStrategy() {
 void setHashUrlStrategy() {
   setUrlStrategy(HashUrlStrategy());
 }
+
+void setNoPathUrlStrategy() {
+  setUrlStrategy(XDPathUrlStrategy());
+}
+
+class XDPathUrlStrategy extends HashUrlStrategy {
+  /// Creates an instance of [PathUrlStrategy].
+  ///
+  /// The [PlatformLocation] parameter is useful for testing to mock out browser
+  /// interactions.
+  XDPathUrlStrategy([
+    super.platformLocation,
+  ]) : _basePath = stripTrailingSlash(extractPathname(checkBaseHref(
+    platformLocation.getBaseHref(),
+  )));
+
+  final String _basePath;
+
+  @override
+  String prepareExternalUrl(String internalUrl) {
+    if (internalUrl.isNotEmpty && !internalUrl.startsWith('/')) {
+      internalUrl = '/$internalUrl';
+    }
+    return '$_basePath/';
+  }
+}
